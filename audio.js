6ad228071bcd4632080e92a2f9239fca28368440
Shubham Gautam <sg2199203@gmail.com>
2021-01-27 10:51:52 +0530

    Adding audio.js to extract audio

--- audio.js
+++ audio.js
@@ -4,7 +4,7 @@ const youtubedl = require('youtube-dl')
 var link = process.argv[2] || 'https://www.youtube.com/watch?v=AZCzfLMHUQs'
 
 //give custom name or the video'll be labelled as 'youtube_video.mp4'
-var title = process.argv[3] || 'youtube_video'
+var title = process.argv[3] || 'youtube_audio'
 
 const video = youtubedl(`${link}`,
     ['--format=18'],
@@ -16,10 +16,10 @@ const video = youtubedl(`${link}`,
 video.on('info', info => {
     downloadVideo(title)
     console.log('Download started')
-    console.log('size: ' + `${info.size/1000} KB`)
+    console.log('size: ' + `${info.size / 1000} KB`)
 })
 
 async function downloadVideo(title) {
-    await video.pipe(fs.createWriteStream(`${title}.mp4`))
+    await video.pipe(fs.createWriteStream(`${title}.mp3`))
     console.log('Download completed');
 }
